e3d1190bdc6b0a70ce3de66565a1f10752566197
scienceswork <2227533225@qq.com>
2016-07-20 15:34:25 +0800

    o2o market front

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webFrontApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the webFrontApp
+ */
+angular.module('webFrontApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webFrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the webFrontApp
+ */
+angular.module('webFrontApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
